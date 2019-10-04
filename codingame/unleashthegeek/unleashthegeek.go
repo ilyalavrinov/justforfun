c4f84d702553a5559dec1e1409a0ea7a021f5f16
Ilya Lavrinov <ilya.v.lavrinov@gmail.com>
2019-10-05 00:31:14 +0300

    Simple scanning

--- codingame/unleashthegeek/unleashthegeek.go
+++ codingame/unleashthegeek/unleashthegeek.go
@@ -33,23 +33,24 @@ const (
 	CARRYORE     = 4
 )
 
-func cmdRequest(i item) string {
-	return fmt.Sprintf("%s %s", REQUEST, i)
+func cmdRequest(i item) {
+	fmt.Printf("%s %s\n", REQUEST, i)
 }
 
-func cmdMove(x, y int) string {
-	return fmt.Sprintf("%s %d %d", MOVE, x, y)
+func cmdMove(x, y int) {
+	fmt.Printf("%s %d %d\n", MOVE, x, y)
 }
 
-func cmdDig(x, y int) string {
-	return fmt.Sprintf("%s %d %d", DIG, x, y)
+func cmdDig(xy coord) {
+	fmt.Printf("%s %d %d\n", DIG, xy.x, xy.y)
 }
 
 func cmdWait() {
-	fmt.Print(WAIT)
+	fmt.Println(WAIT)
 }
 
 func log(args ...interface{}) {
+	args = append(args, "\n")
 	fmt.Fprint(os.Stderr, args...)
 }
 
@@ -66,7 +67,119 @@ type entity struct {
 	id    string
 	what  int
 	carry int
+	xy    coord
 }
+
+type strategy interface {
+	turn(*state)
+}
+
+type scanningTask struct {
+	y           int
+	toHQ        bool
+	rowFinished bool
+}
+
+type scanningStrategy struct {
+	visitedY      map[int]struct{}
+	assignedTasks map[string]*scanningTask
+}
+
+func newScanningStrategy() *scanningStrategy {
+	stg := &scanningStrategy{
+		visitedY:      make(map[int]struct{}),
+		assignedTasks: make(map[string]*scanningTask),
+	}
+	return stg
+}
+
+func (stg *scanningStrategy) turn(s *state) {
+	log("len entities ", len(s.entities))
+	for _, e := range s.entities {
+		log("ET ", e.what, " id ", e.id)
+		if e.what != TYPEMYROBOT {
+			continue
+		}
+		xy := e.xy
+		task, found := stg.assignedTasks[e.id]
+		if !found {
+			log("need new task")
+			task = stg.newTask(s, e, xy, nil)
+		}
+		if task.toHQ {
+			log("toHQ!")
+			if xy.x == 0 {
+				task.toHQ = false
+				if task.rowFinished {
+					log("row finished; need new task")
+					task = stg.newTask(s, e, xy, task)
+				}
+			} else {
+				cmdMove(0, xy.y)
+				continue
+			}
+		}
+		if xy.y != task.y {
+			log("change y")
+			cmdMove(xy.x, task.y)
+			continue
+		}
+		if e.carry == CARRYORE {
+			log("carry ore, fallback")
+			cmdMove(0, xy.y)
+			continue
+		}
+		c := s.field[xy]
+		if c.ore > 0 {
+			log("digging ore")
+			cmdDig(xy)
+			continue
+		}
+		if !c.hole && xy.x != 0 {
+			log("digging test hole")
+			cmdDig(xy)
+			continue
+		}
+		if xy.x == s.width-1 {
+			log("row finished, back to HQ")
+			task.toHQ = true
+			task.rowFinished = true
+			cmdMove(0, xy.y)
+			continue
+		}
+		log("next step")
+		cmdMove(xy.x+1, xy.y)
+	}
+}
+
+func (stg *scanningStrategy) newTask(s *state, myRobot entity, xy coord, prevTask *scanningTask) *scanningTask {
+	yToCheck := []int{xy.y}
+	inc := 1
+	for xy.y-inc >= 0 || xy.y+inc < s.height {
+		if xy.y-inc >= 0 {
+			yToCheck = append(yToCheck, xy.y-inc)
+		}
+		if xy.y+inc < s.height {
+			yToCheck = append(yToCheck, xy.y-inc)
+		}
+		inc++
+	}
+	var task *scanningTask
+	for _, y := range yToCheck {
+		if _, found := stg.visitedY[y]; !found {
+			task = &scanningTask{
+				y:    y,
+				toHQ: true,
+			}
+			stg.visitedY[y] = struct{}{}
+			stg.assignedTasks[myRobot.id] = task
+			break
+		}
+	}
+	return task
+}
+
+var _ strategy = (*scanningStrategy)(nil)
 
 type state struct {
 	width, height int
@@ -74,16 +187,21 @@ type state struct {
 	myScore, enemyScore int
 
 	field    map[coord]cell
-	entities map[coord]entity
+	entities []entity
 
 	radarCooldown, trapCooldown int
-	scanner                     *bufio.Scanner
+
+	step int
+	stg  strategy
+
+	scanner *bufio.Scanner
 }
 
 func newState() *state {
 	s := state{}
 	s.field = make(map[coord]cell, s.height*s.width)
-	s.entities = make(map[coord]entity, s.height*s.width)
+	s.entities = make([]entity, 0)
+	s.stg = newScanningStrategy()
 
 	s.scanner = bufio.NewScanner(os.Stdin)
 	s.scanner.Buffer(make([]byte, 1000000), 1000000)
@@ -95,6 +213,8 @@ func newState() *state {
 }
 
 func (s *state) update() {
+	s.field = make(map[coord]cell, s.width*s.height)
+	s.entities = make([]entity, 0, len(s.entities))
 	s.scanner.Scan()
 	fmt.Sscan(s.scanner.Text(), &s.myScore, &s.enemyScore)
 
@@ -125,21 +245,18 @@ func (s *state) update() {
 	s.scanner.Scan()
 	fmt.Sscan(s.scanner.Text(), &entityCount, &s.radarCooldown, &s.trapCooldown)
 	for i := 0; i < entityCount; i++ {
-		// id: unique id of the entity
-		// type: 0 for your robot, 1 for other robot, 2 for radar, 3 for trap
-		// y: position of the entity
-		// item: if this entity is a robot, the item it is carrying (-1 for NONE, 2 for RADAR, 3 for TRAP, 4 for ORE)
 		e := entity{}
 		s.scanner.Scan()
 		var x, y int
-		fmt.Sscan(s.scanner.Text(), &e.id, &e.what, &e.carry)
+		fmt.Sscan(s.scanner.Text(), &e.id, &e.what, &x, &y, &e.carry)
 
-		s.entities[coord{x, y}] = e
+		e.xy = coord{x, y}
+		s.entities = append(s.entities, e)
 	}
 }
 
 func (s *state) turn() {
-	cmdWait()
+	s.stg.turn(s)
 }
 
 func main() {
