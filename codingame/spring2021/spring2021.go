a7a1104595a805ef9e5916d1af96cf5934225cca
Ilya Lavrinov <ilya.v.lavrinov@gmail.com>
2021-05-13 00:07:28 +0300

    Greedy euristical grow-seed-complete
    Just pushing buttons to make it work somehow. No optiization whatsoever

--- codingame/spring2021/spring2021.go
+++ codingame/spring2021/spring2021.go
@@ -9,6 +9,8 @@ import (
 const (
 	priceCompleteSun       = 4
 	priceCompleteNutrients = 1
+
+	maxDays = 24
 )
 
 func debug(args ...interface{}) {
@@ -90,7 +92,7 @@ func (c cmdSeed) Do() {
 func numTreesBySize(state GameState, size int) int {
 	n := 0
 	for _, t := range state.trees {
-		if t.isMine {
+		if !t.isMine {
 			continue
 		}
 		if t.size != size {
@@ -120,8 +122,9 @@ func costGrow(state GameState, sizeFrom int) int {
 }
 
 type Cell struct {
-	index    int
-	richness int
+	index      int
+	richness   int
+	neighbours []int
 }
 
 type Field struct {
@@ -139,9 +142,22 @@ func NewField(scanner *bufio.Scanner) Field {
 		var index, richness, neigh0, neigh1, neigh2, neigh3, neigh4, neigh5 int
 		scanner.Scan()
 		fmt.Sscan(scanner.Text(), &index, &richness, &neigh0, &neigh1, &neigh2, &neigh3, &neigh4, &neigh5)
+		addNeigh := func(l *[]int, n int) {
+			if n != -1 {
+				*l = append(*l, n)
+			}
+		}
+		neighbours := make([]int, 0, 6)
+		addNeigh(&neighbours, neigh0)
+		addNeigh(&neighbours, neigh1)
+		addNeigh(&neighbours, neigh2)
+		addNeigh(&neighbours, neigh3)
+		addNeigh(&neighbours, neigh4)
+		addNeigh(&neighbours, neigh5)
 		cells = append(cells, Cell{
-			index:    index,
-			richness: richness,
+			index:      index,
+			richness:   richness,
+			neighbours: neighbours,
 		})
 	}
 	field.cells = cells
@@ -170,6 +186,25 @@ type GameState struct {
 	trees         []Tree
 
 	numberOfPossibleMoves int
+}
+
+func (state GameState) CellAt(ix int) Cell {
+	for _, c := range state.field.cells {
+		if c.index == ix {
+			return c
+		}
+	}
+	panic("Cell not found")
+}
+
+func (state GameState) TreeAt(ix int) *Tree {
+	for _, t := range state.trees {
+		if t.cellIndex == ix {
+			t2 := t
+			return &t2
+		}
+	}
+	return nil
 }
 
 func NewGameState(field Field, scanner *bufio.Scanner) GameState {
@@ -224,11 +259,18 @@ func NewGameState(field Field, scanner *bufio.Scanner) GameState {
 
 func greedyComplete(state GameState) Command {
 	debug("greedy complete start")
+	if priceCompleteSun > state.mySun {
+		debugf("greedy complete no sun: %d\n", state.mySun)
+		return nil
+	}
 	bestIx := state.field.numberOfCells + 1
 	bestScore := 0
 	for _, t := range state.trees {
 		debugf("new tree check: cellIndex %d; mine: %t\n", t.cellIndex, t.isMine)
 		if !t.isMine {
+			continue
+		}
+		if t.isDormant {
 			continue
 		}
 		if t.size != 3 {
@@ -246,24 +288,33 @@ func greedyComplete(state GameState) Command {
 	if bestIx < state.field.numberOfCells+1 {
 		res = NewCmdComplete(bestIx)
 	}
-	debugf("greedy complete finish res=%+v", res)
+	debugf("greedy complete finish res=%+v\n", res)
 	return res
 }
 
 func greedyGrow(state GameState) Command {
 	debug("greedy grow start")
 	bestIx := state.field.numberOfCells + 1
-	bestSize := 0
+	bestSize := -1
+
 	for _, t := range state.trees {
 		debugf("new tree check: cellIndex %d; mine: %t\n", t.cellIndex, t.isMine)
 		if !t.isMine {
 			continue
 		}
+		if t.isDormant {
+			continue
+		}
 		if t.size == 3 {
 			continue
 		}
+		cost := costGrow(state, t.size)
+		if cost > state.mySun {
+			debugf("greedy grow no sun; size %d index %d: cost %d sun %d\n", t.size, t.cellIndex, cost, state.mySun)
+			continue
+		}
 		if t.size > bestSize {
-			debugf("New best size %d on cell %d (was score %d cell %d)\n", t.size, t.cellIndex, bestSize, bestIx)
+			debugf("New best size %d on cell %d (was size %d cell %d)\n", t.size, t.cellIndex, bestSize, bestIx)
 			bestIx = t.cellIndex
 			bestSize = t.size
 		}
@@ -273,7 +324,69 @@ func greedyGrow(state GameState) Command {
 	if bestIx < state.field.numberOfCells+1 {
 		res = NewCmdGrow(bestIx)
 	}
-	debugf("greedy grow finish res=%+v", res)
+	debugf("greedy grow finish res=%+v\n", res)
+	return res
+}
+
+func reachableCells(state GameState, from int, size int) []Cell {
+	return doReachableCells(state, from, size, make(map[int]bool, len(state.field.cells)))
+}
+
+func doReachableCells(state GameState, from int, size int, visited map[int]bool) []Cell {
+	if size == 0 {
+		return nil
+	}
+	visited[from] = true
+	c := state.CellAt(from)
+	res := make([]Cell, 0, len(c.neighbours))
+	for _, n := range c.neighbours {
+		if visited[n] {
+			continue
+		}
+		visited[n] = true
+		res = append(res, state.CellAt(n))
+		res = append(res, doReachableCells(state, n, size-1, visited)...)
+	}
+	return res
+}
+
+func greedySeed(state GameState) Command {
+	debug("greedy seed start")
+	cost := costSeed(state)
+	if cost > state.mySun {
+		debugf("greedy seed no sun; need %d, have %d\n", cost, state.mySun)
+		return nil
+	}
+
+	bestIxTo := state.field.numberOfCells + 1
+	bestIxFrom := state.field.numberOfCells + 1
+	bestRichness := 0
+	for _, t := range state.trees {
+		if !t.isMine {
+			continue
+		}
+		if t.isDormant {
+			continue
+		}
+		canReach := reachableCells(state, t.cellIndex, t.size)
+		for _, c := range canReach {
+			if t := state.TreeAt(c.index); t != nil {
+				continue
+			}
+			if c.richness > bestRichness {
+				debugf("New best richness %d on cell %d (was richness %d cell %d)\n", c.richness, c.index, bestRichness, bestIxTo)
+				bestRichness = c.richness
+				bestIxTo = c.index
+				bestIxFrom = t.cellIndex
+			}
+		}
+	}
+
+	var res Command
+	if bestIxTo < state.field.numberOfCells+1 {
+		res = NewCmdSeed(bestIxFrom, bestIxTo)
+	}
+	debugf("greedy seed finish res=%+v\n", res)
 	return res
 }
 
@@ -289,12 +402,21 @@ func main() {
 			scanner.Buffer(make([]byte, 1000000), 1000000)
 		}
 		state := NewGameState(field, scanner)
-		debugf("new day; cells: %d, trees: %d\n", len(state.field.cells), len(state.trees))
-		c := greedyComplete(state)
+		debugf("new day %d; cells: %d, trees: %d\n", state.day, len(state.field.cells), len(state.trees))
+		c := greedyGrow(state)
 		if c == nil {
-			c = greedyGrow(state)
+			seedN := numTreesBySize(state, 0)
+			debugf("seeds %d\n", seedN)
+			if seedN <= (maxDays-state.day)/6 {
+				c = greedySeed(state)
+			}
 			if c == nil {
-				c = NewCmdWait()
+				if numTreesBySize(state, 3) > 1 || state.day == maxDays {
+					c = greedyComplete(state)
+				}
+				if c == nil {
+					c = NewCmdWait()
+				}
 			}
 		}
 
